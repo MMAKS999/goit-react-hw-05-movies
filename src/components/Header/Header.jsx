909da0c2fec4d3fa4f62293a7bf74763d4847c5f
MAX999 <max999b@gmail.com>
2023-07-06 02:52:41 +0300

    2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,22 @@
-// import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 const Header = () => {
   return (
-    <div></div>
-    // <nav className="navbar  navbar-expand-lg bg-body-tertiary">
-    //   <div className="container-fluid">
-    //     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-    //       <div className="navbar-nav">
-    //         <NavLink className="nav-link " aria-current="page" to="/">
-    //           Home
-    //         </NavLink>
-    //         <NavLink className="nav-link" to="/movies">
-    //           Movies
-    //         </NavLink>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </nav>
+    <nav className="navbar  navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <NavLink className="nav-link " aria-current="page" to="/">
+              Home
+            </NavLink>
+            <NavLink className="nav-link" to="/movies">
+              Movies
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
 export default Header;
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Suspense } from 'react';
 import Header from '../header/Header';
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 const Layout = () => {
   return (
