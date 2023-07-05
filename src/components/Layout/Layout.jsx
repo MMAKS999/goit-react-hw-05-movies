015ddf2ee79fc8c7221b253951eee7c8ecd2af3f
MAX999 <max999b@gmail.com>
2023-07-06 02:54:22 +0300

    3

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import Header from '../header/Header';
+// import Header from '../header/Header';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 const Layout = () => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      <nav className="navbar  navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <NavLink className="nav-link " aria-current="page" to="/">
+                Home
+              </NavLink>
+              <NavLink className="nav-link" to="/movies">
+                Movies
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </nav>
       <Suspense fallback={<div>Loading...</div>}>
-
-      <Outlet />
+        <Outlet />
       </Suspense>
     </>
   );
